45fc76dabd6c18961ac38a821ee383c185680d18
terinki777 <102956253+terinki777@users.noreply.github.com>
2022-08-04 15:58:37 +0300

    Update test_randommeet.py

--- test/test_randommeet.py
+++ test/test_randommeet.py
@@ -1,55 +1,37 @@
 import datetime
-import unittest
 import randommeet as rm
 from event import Event
-from person import Person
+
+rand = rm.RandomMeet()
 
 
-class RandomMeetTest(unittest.TestCase):
+def test_rand_date():
+    r = rand.rand_date('2022-12-12', '2024-12-11')
 
-    def test_save_to_json(self):
-        rand = rm.RandomMeet()
-        r = len(rand.save_to_json(5))
+    assert type(r) is datetime.datetime, True
 
-        self.assertEqual(
-            r > 50,
-            True
-        )
 
-    def test_rand_date(self):
-        rand = rm.RandomMeet()
-        r = rand.rand_date()
+def test_place():
+    r = rand.place()
 
-        self.assertEqual(
-            type(r) is datetime.datetime,
-            True
-        )
+    assert len(r) >= 4 or len(r) <= 20, True
 
-    def test_place(self):
-        rand = rm.RandomMeet()
-        r = rand.place()
 
-        self.assertEqual(
-            len(r) >= 5 or len(r) <= 20,
-            True
-        )
+def test_event_title():
+    r = rand.event_title()
 
-    def test_person_list(self):
-        rand = rm.RandomMeet()
-        r = rand.person_list()
-        values = set(item.name for item in Person)
+    assert len(r) >= 5 or len(r) <= 20, True
 
-        self.assertEqual(
-            r.issubset(values),
-            True
-        )
 
-    def test_event_type(self):
-        rand = rm.RandomMeet()
-        r = rand.event_type()
-        values = set(item.name for item in Event)
+def test_person_list():
+    r = rand.person_list()
+    values = ('Alex', 'John', 'Jacob', 'Maria', 'Anna')
 
-        self.assertEqual(
-            r in values,
-            True
-        )
+    assert set(r).issubset(values), True
+
+
+def test_event_type():
+    r = rand.event_type()
+    values = set(item.name for item in Event)
+
+    assert r in values, True
