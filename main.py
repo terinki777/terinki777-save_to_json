eacf09e0e7a50e0665dfd173b563555bd24d4f9d
terinki777 <102956253+terinki777@users.noreply.github.com>
2022-08-04 15:57:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-import datetime
-import random
-import string
+from randommeet import RandomMeet as rm
 
-import pytz
-
-from event import Event
-from person import Person
-
-import randommeet as rm
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    rand = rm()
 
-    rand = rm.RandomMeet()
-    print("random")
-    print(rand.save_to_json(5))
-    print("end")
-
-
-
+    print(rand.save_to_json('2022-12-12', '2024-12-11'))
